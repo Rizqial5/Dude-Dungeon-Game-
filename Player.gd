ff47cddf828033028a9cef9126134a90c5a0f26f
ovisannn <ovi.sanjaya11@gmail.com>
2021-05-10 14:11:36 +0700

    menambahkan rasa yang pernah ada

--- Player.gd
+++ Player.gd
@@ -1,91 +1,130 @@
 extends KinematicBody2D
 
-const UP = Vector2(0,-1)
-const GRAV = 20
-const ACCEL = 50
-const MAX_SPEED = 200
-var motion1 = Vector2()
-var State_play = false
-var attack_num = 1
-var attack_anim = null
-var on_air = false
-var changed_face = true
+export (int) var run_speed = 250
+export (int) var jump_speed = -450
+export (int) var gravity = 1300
 
+var velocity = Vector2()
 
+var attacking = false
+var jumping = false
+var charging = false
 
+var attackState = 3
+
+onready var ani = $AnimatedSprite
 onready var ui = $"UI inGame"
+onready var attkCol_Right = $attackCollusion/right
+onready var attkCol_Left = $attackCollusion/left
+onready var specialR = $attackCollusion/specialR
+onready var specialL = $attackCollusion/specialL
+onready var attackReset = $attackReset
 
+func get_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('move_right')
+	var left = Input.is_action_pressed('move_left')
+	var jump = Input.is_action_just_pressed('jump')
 
-# warning-ignore:unused_argument
-func _process(delta): 
-	motion1.y += GRAV
-	var fall = true
-	var friction = false
-	#Movemement
-	if State_play == false :
-		if Input.is_action_pressed("move_right"):
-			motion1.x = min(motion1.x+ACCEL, MAX_SPEED)
-			if changed_face == false :
-				scale.x = -1
-				changed_face = true
-				ui.set_scale(Vector2(1,1))
-#			$AnimationPlayer.play("Run")
-		elif Input.is_action_pressed("move_left"):
-			if changed_face == true :
-				scale.x = -1
-				changed_face = false
-				ui.set_scale(Vector2(-1,1))
-			motion1.x = max(motion1.x-ACCEL, -MAX_SPEED)
+	if attacking == false:
+		if jump and is_on_floor():
+			jumping = true
+			velocity.y = jump_speed
+			if velocity.y > 0:
+				ani.play("fall")
+			else:
+				ani.play("jump")
 			
-#			$AnimationPlayer.play("Run")
-		else :
-			friction = true
-#			fall = false
-#			$AnimationPlayer.play("Idle")
-#	if on_air == false :
-#		if Input.is_action_just_pressed("attack"):
-#			$Timer.start()
-#			State()
-#			if $Timer.time_left > 0.5 :
-#				attack_num += 1
-#			if attack_num > 2 :
-#				attack_num = 1
+		elif right:
+			velocity.x += run_speed
+			if ani.scale.x < 0:
+				ani.scale.x *= -1
+			if is_on_floor():
+				ani.play("run")
+				
+		elif left:
+			velocity.x -= run_speed
+			if ani.scale.x > 0:
+				ani.scale.x *= -1
+			if is_on_floor():
+				ani.play("run")
+				
+		elif is_on_floor():
+			ani.offset.x = 0
+			ani.play("Idle")
 
+
+func attack():
+	var attack = Input.is_action_pressed("attack")
+	var spcAttack = Input.is_action_pressed("specialAttack")
+	var left = ani.scale.x < 0
+	var right = ani.scale.x > 0
+	
+	if attack and attacking == false:
+		ani.stop()
+		attackReset.start()
+		print(attackState)
+		attacking = true
+		ani.offset.x = 3.403
 		
-	elif scale.x == 1 :
-		ui.set_scale(Vector2(1,1))
+		if right:
+			attkCol_Right.disabled = false
+		elif left:
+			attkCol_Left.disabled = false
+			
+		if attackState == 3:
+			ani.play("at1")
+			attackState -=1
+		elif attackState == 2:
+			ani.play("at2")
+			attackState -=1
+		elif attackState == 1:
+			ani.play("at3")
+			attackState = 3 
 		
+	elif spcAttack and attacking == false:
+		attacking = true
+		ani.offset.x = 19.563
+		ani.play("charge")
 		
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			motion1.y = -500
-		if friction == true :
-			motion1.x = lerp(motion1.x, 0, 0.2) 
-		on_air = false
-	else:
-		if motion1.y < 0:
-			on_air = true
-#			$AnimationPlayer.play("Jump") 
-		 
-		else :
-#			$AnimationPlayer.play("Fall")
-			if friction == true :
-				motion1.x = lerp(motion1.x, 0, 0.05) 
+	elif charging == true:
+		ani.play("spcAt")
+		if right:
+			specialR.disabled = false
+		if left:
+			specialL.disabled = false
+	elif attacking == false:
+		ani.offset.x = 0
+		ani.offset.y = 0
 		
 	
-	motion1 = move_and_slide(motion1, UP )
-	pass
-func State():
-	State_play = true
-	motion1.x = 0
-#	$AnimationPlayer.play(attack_anim)
-#	yield($AnimationPlayer, "animation_finished")
-	State_play = false
-
-	
-
-#func _on_Timer_timeout():
-#	attack_num = 1
-#	pass # Replace with function body.
+func _physics_process(delta):
+	get_input()
+	attack()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	if velocity.y >0:
+		ani.play("fall")
 
 
+func _on_AnimatedSprite_animation_finished():
+	if ani.animation == "charge":
+		charging = true
+		
+	if ani.animation == "spcAt":
+		attacking = false
+		charging = false
+		specialR.disabled = true
+		specialL.disabled = true
+		
+	if ani.animation == "at1" || ani.animation == "at2" || ani.animation == "at3":
+		ani.offset.x = 0
+		attacking = false
+		attkCol_Right.disabled = true
+		attkCol_Left.disabled = true
+		
+
+func _on_attackReset_timeout():
+	attackState = 3
